b4a7bf2f674850613273934bf6fc05f23f93d502
sintez <Sin1984tez17>
2019-10-13 21:32:40 +0300

    3 задачи из ДЗ

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,25 @@
+
+
+public class Variable{
+         public static void main(String[]args){
+
+        byte b=127;
+        short st=12419;
+        char ch='A';
+        int integer=24;
+        long lg=124124L;
+        float fl=12.345949f;
+        double dl=4323.45345;
+        boolean bool=true;
+
+        System.out.println(b);
+        System.out.println(st);
+        System.out.println(ch);
+        System.out.println(integer);
+        System.out.println(lg);
+        System.out.println(fl);
+        System.out.println(dl);
+        System.out.println(bool);
+
+        }
+}
